97fa36a68d1b691203aace28d15449aef214eeb8
retiq <idzkowskit@gmail.com>
2026-05-19 18:21:42 +0200

    feat: add initial project structure

---
+++ src/db/db.h
@@ -0,0 +1,1 @@
+// Handles db interactions
---
+++ src/server/handlers.c
@@ -0,0 +1,14 @@
+int handle_buy();
+
+
+int handle_get_stock();
+int handle_list_items();
+int handle_restock();
+
+
+int handle_insert_money();
+int handle_get_balance();
+
+
+int handle_health();
+int handle_reset_machine();
